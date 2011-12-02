9f1d62e4329a77b75e99b9e8c8f7cb036b47ad65
Inderbir Singh <inderbir.singh@r9fc7ra.mkhoj.com>
2011-12-02 17:50:12 +0530

    First checkin for databus

---
+++ src/main/java/com/inmobi/databus/HDFSException.java
@@ -0,0 +1,17 @@
+package com.inmobi.databus;
+
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: inderbir.singh
+ * Date: 02/12/11
+ * Time: 12:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HDFSException extends  Exception{
+
+    public HDFSException(String message) {
+        super(message);
+    }
+}
