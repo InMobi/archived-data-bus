828857fd4bb79d36faf3c060addd1de72657b889
Inder Singh <inder.pall@gmail.com>
2011-12-19 19:37:36 +0530

    merge for remote copier from head

---
+++ src/main/java/com/inmobi/databus/consume/CopyMapper.java
@@ -0,0 +1,11 @@
+package com.inmobi.databus.consume;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: inderbir.singh
+ * Date: 19/12/11
+ * Time: 7:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CopyMapper {
+}
