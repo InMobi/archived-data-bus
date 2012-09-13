d9f51d34baaf8f38933c67fea3b0b833f8e1e5b3
raju <raju.bairishetti@inmobi.com>
2012-09-13 15:17:41 +0530

    new file:   databus-worker/src/main/java/com/inmobi/databus/utils/OrderlyCreationOfDirs.java 	new file:   tools/scripts/databus-utils.sh

---
+++ databus-worker/src/main/java/com/inmobi/databus/utils/OrderlyCreationOfDirs.java
@@ -0,0 +1,119 @@
+package com.inmobi.databus.utils;
+
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import com.inmobi.databus.utils.CalendarHelper;
+
+
+public class OrderlyCreationOfDirs {
+
+  private static final Log LOG = LogFactory.getLog(
+      OrderlyCreationOfDirs.class);
+
+  public OrderlyCreationOfDirs() {
+  }
+
+  public void doRecursiveListing(Path dir, Set<Path> listing, 
+      FileSystem fs) throws IOException {
+    FileStatus[] fileStatuses = fs.listStatus(dir);
+    if (fileStatuses == null || fileStatuses.length == 0) {
+      LOG.debug("No files in directory:" + dir);
+      listing.add(dir);
+    } else {
+      for (FileStatus file : fileStatuses) {  
+        if (file.isDir()) {
+          doRecursiveListing(file.getPath(), listing,  fs);	      
+        } else {
+          listing.add(file.getPath().getParent());
+        }       
+      } 
+    }
+  }
+
+  public void validateOrderlyCreationOfPaths(
+      TreeMap<Date , FileStatus> creationTimeOfFiles, 
+      List<Path> outOfOrderDirs) {
+    Date previousKeyEntry = null;
+    for (Date presentKeyEntry : creationTimeOfFiles.keySet() ) {
+      if (previousKeyEntry != null) {
+        //LOG.info(previousKeyEntry);
+        if (creationTimeOfFiles.get(previousKeyEntry).getModificationTime()
+            > creationTimeOfFiles.get(presentKeyEntry).getModificationTime()) {
+          System.out.println("Directory is created in out of order :    " + 
+              creationTimeOfFiles.get(previousKeyEntry).getPath()); 
+          outOfOrderDirs.add(creationTimeOfFiles.get(previousKeyEntry)
+              .getPath());
+          previousKeyEntry = presentKeyEntry;
+        }
+        
+      }
+      previousKeyEntry = presentKeyEntry;
+    }
+  }
+
+  public void listingAndValidation(Path streamDir, FileSystem fs, 
+      List<Path> outOfOrderDirs)
+      throws IOException {
+    Set<Path> listing = new HashSet<Path>();
+    TreeMap<Date, FileStatus>creationTimeOfFiles = new TreeMap<Date, 
+        FileStatus >();
+    doRecursiveListing(streamDir, listing, fs);
+    for (Path path :listing) {
+      // LOG.info(path);
+      creationTimeOfFiles.put(CalendarHelper.getDateFromStreamDir(
+          streamDir, path), fs.getFileStatus(path));
+    }
+    validateOrderlyCreationOfPaths(creationTimeOfFiles, outOfOrderDirs);
+  }
+
+  public List<Path> pathConstruction(String rootDirs[] , String baseDirs[] , 
+      String streamNames[]) throws IOException{
+    List<Path> outOfOrderDirs = new ArrayList<Path>();
+    for (String rootDir : rootDirs) {
+      FileSystem fs = new Path(rootDir).getFileSystem(new Configuration());
+      for (String baseDir : baseDirs) {
+        Path rootBaseDirPath = new Path(rootDir, baseDir);
+        for (String streamName : streamNames) {
+          Path streamDir = new Path(rootBaseDirPath , streamName); 
+          FileStatus[] files = fs.listStatus(streamDir);
+          if (files == null || files.length == 0) {
+            break;
+          }
+          listingAndValidation(streamDir, fs , outOfOrderDirs);
+          
+        }    
+      }
+    }
+    LOG.info("size of :" + outOfOrderDirs.size());
+    return outOfOrderDirs;
+  }
+
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) throws IOException,ParseException {
+    String[] rootDirs     =   args[0].split(",");
+    String[] baseDir      =   args[1].split(",");
+    String[] streamName   =   args[2].split(",");
+
+    OrderlyCreationOfDirs obj = new 
+        OrderlyCreationOfDirs();
+    //construct all paths
+    obj.pathConstruction(rootDirs, baseDir, streamName);
+  }
+}
---
+++ tools/scripts/databus-utils.sh
@@ -0,0 +1,137 @@
+# constants
+DATABUS_MIRROR_STREAM_VALIDATION_CLASS="com.inmobi.databus.utils.MirrorStreamDataConsistencyValidation"
+DATABUS_ORDERLY_CREATION_FILES_CLASS="com.inmobi.databus.utils.OrderlyCreationOfDirsForDiffStreams"
+
+#functions
+info() {
+  local msg=$1
+
+  echo "Info: $msg" >&2
+}
+
+warn() {
+  local msg=$1
+
+  echo "Warning: $msg" >&2
+}
+
+error() {
+  local msg=$1
+  local exit_code=$2
+
+  echo "Error: $msg" >&2
+
+  if [ -n "$exit_code" ] ; then
+    exit $exit_code
+  fi
+}
+
+display_help() {
+  cat <<EOF
+USAGE: $0 mirrorstreamdataconsistency <mergedstreamroot-dir> <streamname(comma separated list)> <mirrorstreamroot-dir>
+       $0 orderlycreated <root-dirs (comma separated list)>  <basedir (comma separated list) <streamname (comma separated list) >
+
+EOF
+}
+
+run_databus() {
+  local DATABUS_UTILITY_CLASS
+
+  if [ "$#" -gt 0 ]; then
+    DATABUS_UTILITY_CLASS=$1
+    shift
+  else
+    error "Must specify databus utility class" 1
+  fi
+
+  set -x
+  exec $JAVA_HOME/bin/java $JAVA_OPTS -cp "$DATABUS_CLASSPATH" \
+      "$DATABUS_UTILITY_CLASS" $*
+
+}
+
+################################
+# main
+################################
+
+# set default params
+DATABUS_CLASSPATH=""
+DATABUS_JAVA_LIBRARY_PATH=""
+JAVA_OPTS="-Xmx128M"
+
+
+mode=$1
+shift
+
+case "$mode" in
+  help)
+    display_help
+    exit 0
+    ;;
+  mirrorstreamdataconsistency)
+    opt_mirror=1
+    ;;
+  orderlycreated)
+    opt_order=1;
+    ;;
+  *)
+    error "Unknown or unspecified command '$mode'"
+    echo
+    display_help
+    exit 1
+    ;;
+esac
+
+while [ -n "$*" ] ; do
+  arg=$1
+  shift
+
+  case "$arg" in
+    -D*)
+      JAVA_OPTS="${JAVA_OPTS} $arg"
+      ;;
+    *)
+      args="$args $arg"
+      ;;
+  esac
+done
+
+
+#find java
+if [ -z "${JAVA_HOME}" ] ; then
+  echo "Warning: JAVA_HOME not set!"
+    JAVA_DEFAULT=`type -p java`
+    [ -n "$JAVA_DEFAULT" ] || error "Unable to find java executable. Is it in your PATH?" 1
+    JAVA_HOME=$(cd $(dirname $JAVA_DEFAULT)/..; pwd)
+fi
+
+[ -n "${JAVA_HOME}" ] || error "Unable to find a suitable JAVA_HOME" 1
+
+
+# figure out where the client distribution is
+if [ -z "${DATABUS_HOME}" ] ; then
+  DATABUS_HOME=$(cd $(dirname $0)/..; pwd)
+fi
+
+DEV_CLASSPATH=${DATABUS_HOME}/databus-core/target/classes
+DEV_CLASSPATH=$DEV_CLASSPATH:${DATABUS_HOME}/databus-worker/target/classes
+ln -s /home/rajubairishetti/.m2/repository/com/inmobi/databus/* /home/rajubairishetti/data-bus/databus-dist/target/databus-dist-1.2.0-SNAPSHOT-pkg/lib/* 
+# Append to the classpath
+if [ -n "${DATABUS_CLASSPATH}" ] ; then
+  DATABUS_CLASSPATH="${DATABUS_HOME}/lib/*:$DEV_CLASSPATH:$DATABUS_CLASSPATH"
+else
+  DATABUS_CLASSPATH="${DATABUS_HOME}/lib/*:$DEV_CLASSPATH"
+fi
+
+# finally, invoke the appropriate command
+if [ -n "$opt_mirror" ] ; then
+  run_databus $DATABUS_MIRROR_STREAM_VALIDATION_CLASS $args $args1 $args2
+elif [ -n "$opt_order" ] ; then
+  run_databus $DATABUS_ORDERLY_CREATION_FILES_CLASS $args 
+#  echo $args
+else
+  error "This message should never appear" 1
+fi
+
+exit 0
+
