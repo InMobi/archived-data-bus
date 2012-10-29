313115b6663d4142c9d9da615949e83677c520bf
Inderbir <inderbir@databusdev1.mkhoj.com>
2012-10-29 05:03:03 -0700

    Adding filestore stats : num_bytes_written, num_events_written per category

--- src/store.cpp
+++ src/store.cpp
@@ -558,8 +558,10 @@ void FileStoreBase::printStats() {
 
   string log_str = msg.str();
   LOG_OPER("[%s]", log_str.c_str());
-
   /*stats_file->write(msg.str());*/
+  // update the stats
+  g_Handler->incCounter(categoryHandled, fsType + ":wrote num messages", eventsWritten);
+  g_Handler->incCounter(categoryHandled, fsType + ":wrote bytes", currentSize);  
   stats_file->close();
 }
 
